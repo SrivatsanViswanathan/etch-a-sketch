0418b9828f957ce34b05880534b0a26b5ec5ec6c
Srivatsan Viswanathan <sriv.viswanathan@gmail.com>
2022-05-13 16:21:35 -0400

    Updated Javascript file
    - Comments were added for the code.
    - Some function variables were moved to global variables as they were used multiple times in different functions.

--- script.js
+++ script.js
@@ -1,136 +1,132 @@
-var squares = 8*8;
+//Global variables
+var squares = 8 * 8;
+
+const grid = document.getElementById('grid');
+const gridSubmit = document.getElementById('grid-submit');
+const defaultColour = document.getElementById('default');
+const shadedColour = document.getElementById('shaded');
+const randomColour = document.getElementById('random');
+const eraser = document.getElementById('eraser');
+const gridErase = document.getElementById('grid-erase');
+
+const modal = document.getElementById('modal');
+const submit = document.getElementById('submit');
+const close = document.getElementById('close');
 
 gridCreate(squares);
 gridUpdate();
 
+// Create grid based on screen height and width
+// Create grid squares based on default square amount OR user input
 function gridCreate(squares) {
- const grid = document.getElementById('grid');
+    grid.textContent = '';
 
- grid.textContent = '';
+    var height = Math.floor(grid.offsetHeight);
+    var width = Math.floor(grid.offsetWidth);
+    var borderLength = 2;
 
- var height = Math.floor(grid.offsetHeight);
- var width = Math.floor(grid.offsetWidth);
- var gridSize = height*width;
- 
- var borderLength = 2;
+    var gridSize = height * width;
+    var squareSize = Math.sqrt(gridSize / squares) - borderLength;
 
- const squareSize = Math.sqrt(gridSize/squares) - borderLength;
-
- for (var i = 0; i < squares; i++) {
-  const gridSquare = document.createElement('div')
-  gridSquare.classList.add('grid-squares');
-  gridSquare.setAttribute('id', 'grid-squares');
-  gridSquare.style.height = squareSize + 'px';
-  gridSquare.style.width = squareSize + 'px';
-  grid.append(gridSquare);
-  }
-  gridDraw();
-}
-
-function gridDraw() {
-
-  const defaultColour = document.getElementById('default');
-  const shadedColour = document.getElementById('shaded');
-  const randomColour = document.getElementById('random');
-  const eraser = document.getElementById('eraser');
-  const gridSquare = document.querySelectorAll('.grid-squares');
-
-  gridSquare.forEach(element => {
-    element.addEventListener('mouseover', function(e) {
-      e.target.style.backgroundColor = 'black';
-    });
-  });
-
-  defaultColour.addEventListener('click', function(e) {
-    gridSquare.forEach(element => {
-      element.addEventListener('mouseover', function(e) {
-        e.target.style.backgroundColor = 'black';
-      });
-    });
-  });
-
-  shadedColour.addEventListener('click', function (e) {
-    gridSquare.forEach(element => {
-      var shadedOne = 255;
-      var shadedTwo = 255;
-      var shadedThree = 255;
-      element.addEventListener('mouseover', function (e) {
-        if (shadedOne >= 0 && shadedTwo >= 0 && shadedThree >= 0) {
-          shadedOne = shadedOne - 25.5;
-          shadedTwo = shadedTwo - 25.5;
-          shadedThree = shadedThree - 25.5;
-          console.log('hello');
-        }
-        e.target.style.backgroundColor = 'rgb(' + shadedOne + ',' + shadedTwo + ',' + shadedThree + ')';
-      });
-    });
-  });
-
-  randomColour.addEventListener('click', function(e) {
-    gridSquare.forEach(element => {
-      element.addEventListener('mouseover', function(e) {
-        var one = Math.floor(Math.random() * (255));
-        var two = Math.ceil(Math.random() * 255);
-        var three = Math.floor(Math.random() * 255);
-        e.target.style.backgroundColor = 'rgb(' + one + ',' + two + ',' + three + ')';
-      });
-    });
-  });
-
-  eraser.addEventListener('click', function (e) {
-    gridSquare.forEach(element => {
-      element.addEventListener('mouseover', function (e) {
-        e.target.style.backgroundColor = 'white';
-      });
-    });
-  });
-
-  const gridSubmit = document.getElementById('grid-submit');
-
-  gridSubmit.addEventListener('click', modal);
-}
-
-function gridUpdate() {
- 
- const button = document.getElementById('grid-erase');
- const gridSquare = document.querySelectorAll('.grid-squares');
-
- button.addEventListener('click', update)
-
- function update() {
-   gridSquare.forEach(element => {
-     element.classList.remove('active');
-     element.style.backgroundColor = '';
-   });
- };
-
- const submit = document.getElementById('submit');
- const modal = document.getElementById('modal');
- 
- submit.addEventListener('click', () => {
-   const gridNum = document.getElementById('grid-number').value;
-   if (gridNum >= 1 && gridNum <= 100) {
-     squares = gridNum;
-     modal.style.display = 'none';
-     gridCreate(squares * squares);
-   }
- })
-
-}
-
-function modal() {
-  const modal = document.getElementById('modal');
-  modal.style.display = 'block';
-
-  const close = document.getElementById('close');
-
-  close.addEventListener('click', () => {
-    modal.style.display = 'none';
-  });
-
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+    for (var i = 0; i < squares; i++) {
+        const gridSquare = document.createElement('div')
+        gridSquare.classList.add('grid-squares');
+        gridSquare.setAttribute('id', 'grid-squares');
+        gridSquare.style.height = squareSize + 'px';
+        gridSquare.style.width = squareSize + 'px';
+        grid.append(gridSquare);
     }
-  }
+    gridDraw();
+}
+
+// Draw on grid based on user's menu button selection
+function gridDraw() {
+    const gridSquare = document.querySelectorAll('.grid-squares');
+
+    gridSquare.forEach(element => {
+        element.addEventListener('mouseover', function (e) {
+            e.target.style.backgroundColor = 'black';
+        });
+    });
+
+    defaultColour.addEventListener('click', function (e) {
+        gridSquare.forEach(element => {
+            element.addEventListener('mouseover', function (e) {
+                e.target.style.backgroundColor = 'black';
+            });
+        });
+    });
+
+    shadedColour.addEventListener('click', function (e) {
+        gridSquare.forEach(element => {
+            var shadedOne = 255;
+            var shadedTwo = 255;
+            var shadedThree = 255;
+            element.addEventListener('mouseover', function (e) {
+                if (shadedOne >= 0 && shadedTwo >= 0 && shadedThree >= 0) {
+                    shadedOne = shadedOne - 25.5;
+                    shadedTwo = shadedTwo - 25.5;
+                    shadedThree = shadedThree - 25.5;
+                    console.log('hello');
+                }
+                e.target.style.backgroundColor = 'rgb(' + shadedOne + ',' + shadedTwo + ',' + shadedThree + ')';
+            });
+        });
+    });
+
+    randomColour.addEventListener('click', function (e) {
+        gridSquare.forEach(element => {
+            element.addEventListener('mouseover', function (e) {
+                var one = Math.floor(Math.random() * (255));
+                var two = Math.ceil(Math.random() * 255);
+                var three = Math.floor(Math.random() * 255);
+                e.target.style.backgroundColor = 'rgb(' + one + ',' + two + ',' + three + ')';
+            });
+        });
+    });
+
+    eraser.addEventListener('click', function (e) {
+        gridSquare.forEach(element => {
+            element.addEventListener('mouseover', function (e) {
+                e.target.style.backgroundColor = 'white';
+            });
+        });
+    });
+
+    gridSubmit.addEventListener('click', modalPopUp);
+    gridErase.addEventListener('click', update)
+
+    function update() {
+        gridSquare.forEach(element => {
+            element.classList.remove('active');
+            element.style.backgroundColor = '';
+        });
+    }
+}
+
+// Update grid based on user's grid size specifications
+function gridUpdate() {
+    submit.addEventListener('click', () => {
+        const gridNum = document.getElementById('grid-number').value;
+        if (gridNum >= 1 && gridNum <= 100) {
+            squares = gridNum;
+            modal.style.display = 'none';
+            gridCreate(squares * squares);
+        }
+    });
+}
+
+
+// Modal pop up for updating grid size
+function modalPopUp() {
+    modal.style.display = 'block';
+    close.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 }
